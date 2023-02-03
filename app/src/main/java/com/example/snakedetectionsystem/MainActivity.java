28af1df8a7fa0e412870895a1f5192d5937d26aa
Dinithi Malmida <112160974+DinithiMalmi@users.noreply.github.com>
2023-02-03 07:45:39 +0530

    start page

--- app/src/main/java/com/example/snakedetectionsystem/MainActivity.java
+++ app/src/main/java/com/example/snakedetectionsystem/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.snakedetectionsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
