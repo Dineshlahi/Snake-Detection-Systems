96d3422802ed40fc5d51dfafb7461dee171cc765
Dinithi Malmida <112160974+DinithiMalmi@users.noreply.github.com>
2023-03-29 11:42:53 +0530

    Main Activity

--- app/src/main/java/com/example/snakedetectionsystem/MainActivity.java
+++ app/src/main/java/com/example/snakedetectionsystem/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.snakedetectionsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
     }
 }
